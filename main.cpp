246cc4a7ac756a585f3a80633e8e0b98fdd5b799
Jaurel Fosset <jf714312@ad.u-bourgogne.fr>
2022-05-11 09:17:07 +0200

    add a collision function

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 #include <QApplication>
 
+bool detecter_collision(int ennemis_x[], int ennemis_y[], int nb_ennemis, int x, int y);
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -9,3 +11,14 @@ int main(int argc, char *argv[])
     w.show();
     return a.exec();
 }
+
+bool detecter_collision(int ennemis_x[], int ennemis_y[], int nb_ennemis, int x, int y)
+{
+	for (unsigned i = 0; i < nb_ennemis; i++)
+	{
+		if (ennemis_x[i] == x && ennemis_y[i] == y)
+			return true;
+	}
+	return false;
+}
+
